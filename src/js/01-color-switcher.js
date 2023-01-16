59737a4f4ecfad8971ccb60e0cb6f61da39da3eb
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-16 17:58:10 +0200

    updated task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtnEl = document.querySelector('[data-start]')
+const stopBtnEl = document.querySelector('[data-stop]')
+const bodyEl = document.querySelector('body')
 
+stopBtnEl.setAttribute('disabled', 'true')
+ let timerId = null
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startHandler = () => {
+    startBtnEl.setAttribute('disabled', 'true')
+    stopBtnEl.removeAttribute('disabled')
+   timerId = setInterval(()=>{ bodyEl.style.backgroundColor = (getRandomHexColor())},1000)
+
+}
+const stopHandler = (event) => {
+    stopBtnEl.setAttribute('disabled', 'true')
+    startBtnEl.removeAttribute('disabled') 
+    clearInterval(timerId)
+}
+
+
+startBtnEl.addEventListener('click', startHandler)
+stopBtnEl.addEventListener('click',stopHandler)
