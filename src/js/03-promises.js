871f87043c33eb917f2e0c6a6b828b6bd8adaa0c
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-18 20:03:54 +0200

    updated task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,40 +1,52 @@
+import Notiflix from "notiflix";
 
 const formEl = document.querySelector('.form')
 
+let delay = null;
+let amount = null;
+let delayStep = null;
 
 
-// function createPromise(position, delay) {
 
-//   const shouldResolve = Math.random() > 0.3;
+function createPromise(position, delay) {
 
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       if (shouldResolve) {
-//     // Fulfill
-//     resolve({position,delay})
-//   } 
-//     // Reject
-//     reject({position,delay})
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+    
+    resolve({position,delay})
+  } 
+    
+    reject({position,delay})
   
-//     },delay)
-//   })
+    },delay)
+  })
   
-// }
+}
 
 
-// const submitHandler = (event) => {
-// event.preventDefault()
-//   amount = Number(formEl.elements.amount.value);
-  
-//  delayStep = +formEl.elements.step.value;
-//  firstDelay = +formEl.elements.delay.value;
-//   console.log(firstDelay, delayStep, amount)
-  
+const submitHandler = (event) => {
+event.preventDefault()
+   amount = Number(formEl.elements.amount.value);
+   delayStep = Number(formEl.elements.step.value);
+   delay = Number(formEl.elements.delay.value);
 
-// }
+    for (let position = 1; position < amount; position += 1){
+        createPromise(position , delayStep)
+            .then(data => Notiflix.Notify.success(`Fulfilled promise ${data.position} in ${data.delay} `))
+            .catch(data => Notiflix.Notify.failure(`Rejected promise ${data.position} in ${data.delay} `))
+        delay += delayStep
+        // console.log(delay)
+    }
+    
+    // console.log(firstDelay, delayStep, amount)
+    
+}
 
 
-// formEl.addEventListener('submit',submitHandler)
+formEl.addEventListener('submit',submitHandler)
 
 
 
