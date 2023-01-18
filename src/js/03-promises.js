45d1236c5437894fb244174a9be0fa03795a5b2c
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-01-18 20:10:12 +0200

    updated counter

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -33,7 +33,7 @@ event.preventDefault()
    delayStep = Number(formEl.elements.step.value);
    delay = Number(formEl.elements.delay.value);
 
-    for (let position = 1; position < amount; position += 1){
+    for (let position = 1; position <= amount; position += 1){
         createPromise(position , delay)
             .then(data => Notiflix.Notify.success(`Fulfilled promise ${data.position} in ${data.delay} `))
             .catch(data => Notiflix.Notify.failure(`Rejected promise ${data.position} in ${data.delay} `))
